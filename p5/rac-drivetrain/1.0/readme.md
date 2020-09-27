11b6c6fe156c127632c3d7694af3d12ff2558259
Maic Lopez Saenz <maic@lopsae.com>
2020-09-27 01:21:06 -0700

    new page and folder for drivetrain drawing

---
+++ p5/rac-drivetrain/1.0/readme.md
@@ -0,0 +1,7 @@
+# Bike Drivetrain
+
+Abstract design of a bicycle drivetrain.
+
+Intender as a likely version 1.0 of RAC.
+
+Development experiment for RAC, a library to express ruler-and-compass constructions.
---
+++ p5/rac-drivetrain/1.0/script.js
@@ -0,0 +1,97 @@
+"use strict";
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
+  noStroke();
+  noFill();
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+function mousePressed(event) {
+  rac.Control.pointerPressed(rac.Point.mouse());
+  redraw();
+}
+
+
+function mouseDragged(event) {
+  rac.Control.pointerDragged(rac.Point.mouse());
+  redraw();
+}
+
+
+function mouseReleased(event) {
+  rac.Control.pointerReleased(rac.Point.mouse());
+  redraw();
+}
+
+
+let angleControl = new rac.ArcControl(1/4, 1);
+angleControl.setValueWithArcLength(1/8);
+angleControl.addMarkerAtCurrentValue();
+rac.Control.controls.push(angleControl);
+
+
+
+function draw() {
+  clear();
+
+  let colorScheme = {
+    background:  new rac.Color( .1,  .1,  .1), // blackish
+    stroke:      new rac.Color( .9,  .2,  .2,  .8), // red,
+    secondary:   new rac.Color( .7,  .3,  .3,  .3), // rose pink
+    fill:        new rac.Color( .8,  .8,  .8,  .7), // whiteish
+    controlFill: new rac.Color( .8,  .8,  .8, 1.0), // whiteish
+    pointer:     new rac.Color( .9,  .9,  .9,  .6), // whiteish
+    highlight:   new rac.Color(  0, 1.0, 1.0,  .8)// cyan
+  };
+  colorScheme.background.applyBackground();
+
+  let mainStroke = colorScheme.stroke.stroke(2);
+  mainStroke.apply();
+
+  let secondaryStroke = colorScheme.secondary.stroke(2);
+
+  // Testing highlight
+  let highlight = colorScheme.highlight.stroke(5);
+
+  let controlStyle = colorScheme.stroke.stroke(3)
+    .styleWithFill(colorScheme.controlFill.fill());
+
+  rac.Control.controls.forEach(item => item.style = controlStyle);
+
+  rac.Control.pointerStyle = colorScheme.pointer.stroke(3);
+
+
+  // Start point
+  let start = new rac.Point(width * 1/4, height * 2/3);
+
+
+  // Angle control
+  angleControl.anchor = start
+    .segmentToAngle(rac.Angle.w, rac.Control.radius * 6)
+    .arcWithEnd(1/4, false);
+
+  angleControl.center()
+    .segmentToPoint(start)
+    .draw(secondaryStroke);
+  angleControl.anchor.startSegment()
+    .reverse()
+    .segmentToBisector()
+    .draw(secondaryStroke);
+
+
+
+  // Controls draw on top
+  rac.Control.drawControls();
+
+
+  console.log(`👑 ~finis coronat opus ${Date.now()}`);
+}
+
